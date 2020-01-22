6c9503f1e4b3d2c360ed33db9089f2b133f92862
ukb <ukwuanibarnbas@gmail.com>
2020-01-22 03:21:24 +0100

    TodoList App Client cleanup

--- server/controller/index.js
+++ server/controller/index.js
@@ -54,7 +54,7 @@ module.exports = {
             res.json({
                 status: 200,
                 success: true,
-                msg: 'Item updated'
+                msg: req.body.data
             })
         })
     },
